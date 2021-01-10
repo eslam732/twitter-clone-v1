c6644d4b07d3ffdcaa7678680354f02309433f21
Ahmed Gamal <agsalamh@gmail.com>
2021-01-10 16:31:20 +0200

    modify Reply Schema By extending the Tweet Schema

--- models/reply.js
+++ models/reply.js
@@ -1,35 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
-    content: {
-        type: String
-    },
-    postOfComment: {
-        type: Schema.Types.ObjectId,
-        ref: "Post"
-    },
-    imageUrl:{
-        type:String
-    },
-    replyOnComment: {
-        type: Schema.Types.ObjectId,
-        ref: "Comment"
-    },
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    },
-    replyies:[{
-        type:Schema.Types.ObjectId,
-        ref:"Comment"
-    }],
-    replyingTo: [{
-        type: String,
-        required: true
-    }]
 
+const {tweetSchema} = require('./tweet');
+// Same Schema as Tweet + main Tweet
 
+const replySchema = tweetSchema.clone();
+
+replySchema.add({
+    mainTweet: {
+        required: true,
+        type: Schema.Types.ObjectId,
+        ref: 'Tweet'
+    }
 });
 
-module.exports = mongoose.model('Comment', commentSchema);
+module.exports = mongoose.model('Reply', replySchema);
